f3040b19256b2338fab779285eecfb2e65f99c5d
Miguel-UNSCH <miguelramirezgonzales48@gmail.com>
2024-12-04 07:58:58 -0500

    sidebar-option

--- src/components/layout/sidebar-options.tsx
+++ src/components/layout/sidebar-options.tsx
@@ -1,23 +1,44 @@
+'use client'
+
 import Link from "next/link";
 import { GoHomeFill } from "react-icons/go";
 import { IoSettings, IoNotifications } from "react-icons/io5";
 import { BsDatabaseFillCheck } from "react-icons/bs";
-
+import { usePathname } from "next/navigation";
 
 function SidebarOptions() {
+
+  const pathname = usePathname()
+
   return (
     <div className="flex flex-col gap-6 items-center">
-      <Link href={'/dashboard'} className="bg-[#CDCDCD] dark:bg-[#40404B] p-2 rounded-full dark:text-white">
-        <GoHomeFill className="text-xl dark:text-white" />
+      <Link href={'/dashboard'} 
+        className={` p-2 rounded-full ${
+          pathname === '/dashboard'? 'bg-[#CDCDCD] dark:bg-[#40404B] dark:text-white' : 'text-[#030303] dark:text-[#8E8EA8]'
+        }`}
+      >
+        <GoHomeFill className="text-xl dark:hover:text-white" />
       </Link>
-      <Link href={'/dashboard/obras'}>
-        <BsDatabaseFillCheck className="text-xl text-[#030303] dark:text-[#8E8EA8]" />
+      <Link href={'/dashboard/obras'} 
+        className={` p-2 rounded-full ${
+          pathname === '/dashboard/obras'? 'bg-[#CDCDCD] dark:bg-[#40404B] dark:text-white' : 'text-[#030303] dark:text-[#8E8EA8]'
+        }`}
+      >
+        <BsDatabaseFillCheck className="text-xl dark:hover:text-white" />
       </Link>
-      <Link href={'/dashboard'}>
-        <IoNotifications className="text-xl text-[#030303] dark:text-[#8E8EA8]" />
+      <Link href={'/dashboard'} 
+        className={` p-2 rounded-full ${
+          pathname === '/dashboard/1'? 'bg-[#CDCDCD] dark:bg-[#40404B] dark:text-white' : 'text-[#030303] dark:text-[#8E8EA8]'
+        }`}
+      >
+        <IoNotifications className="text-xl dark:hover:text-white" />
       </Link>
-      <Link href={'/dashboard'}>
-        <IoSettings className="text-xl text-[#030303] dark:text-[#8E8EA8]" />
+      <Link href={'/dashboard'} 
+        className={` p-2 rounded-full ${
+          pathname === '/dashboard/2'? 'bg-[#CDCDCD] dark:bg-[#40404B] dark:text-white' : 'text-[#030303] dark:text-[#8E8EA8]'
+        }`}
+      >
+        <IoSettings className="text-xl dark:hover:text-white" />
       </Link>
     </div>
   )
